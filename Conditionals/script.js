397d2951271ffdf8c84d622fe59e7f861be344db
ShivamJoker <singhshivamkr@gmail.com>
2018-07-05 15:30:32 +0530

    basics completed

---
+++ Conditionals/script.js
@@ -0,0 +1,16 @@
+new Vue({
+    el: '#app',
+    data: {
+       success: false,
+       error: false
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+});
+
+
+
---
+++ Looping/script.js
@@ -0,0 +1,21 @@
+new Vue({
+    el: '#app',
+    data: {
+       characters: ['cat', 'dog', 'bull', 'horse'],
+       sizes:[
+           {name: 'Roro', inch: 5},
+           {name: 'Bullu', inch: 1},
+           {name: 'Prando', inch: 15},
+           {name: 'Dada', inch: 2}
+       ]
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+});
+
+
+
